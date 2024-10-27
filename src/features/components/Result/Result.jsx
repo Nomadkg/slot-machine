523246db181b3dd9fd7f6737a80565d069c3edde
Nomad <noneprado@gmail.com>
2024-10-27 02:21:46 +0200

    minor refactor

--- src/features/components/Result/Result.jsx
+++ src/features/components/Result/Result.jsx
@@ -2,52 +2,49 @@ import { Link, useLocation } from 'react-router-dom';
 import WheelImage from '../../../common/components/WheelImage';
 import { SORRY_YOU_LOSE, YOU_WIN_HUNDRED_USD, YOU_WIN_TEN_USD, YOU_WIN_TWENTY_USD, PLAY_AGAIN } from '../../../common/constants';
 
-function checkWheels({ firstWheel, secondWheel, thirdWheel }) {
-    let msg;
 
+function returnResult(msg, {firstWheel, secondWheel, thirdWheel}) {
+    return (
+        <>
+            <h1>{msg}</h1>
+            <WheelImage wheels={[firstWheel, secondWheel, thirdWheel]} />
+        </>
+    );
+}
+
+function checkWheels({ firstWheel, secondWheel, thirdWheel }) {
     if (firstWheel === undefined && secondWheel === undefined && thirdWheel === undefined) {
         return null
     }
 
     if (firstWheel !== secondWheel && firstWheel !== thirdWheel && secondWheel !== thirdWheel) {
-        msg = SORRY_YOU_LOSE
+        return returnResult(SORRY_YOU_LOSE, { firstWheel, secondWheel, thirdWheel })
     }
 
     if (firstWheel === secondWheel && firstWheel === thirdWheel) {
-        msg = YOU_WIN_HUNDRED_USD
+        return returnResult(YOU_WIN_HUNDRED_USD, { firstWheel, secondWheel, thirdWheel })
     }
 
     if (firstWheel !== secondWheel && firstWheel === thirdWheel) {
-        msg = YOU_WIN_TEN_USD
+        return returnResult(YOU_WIN_TEN_USD, { firstWheel, secondWheel, thirdWheel })
     }
 
     if (firstWheel === secondWheel && secondWheel !== thirdWheel) {
-        msg = YOU_WIN_TWENTY_USD
+        return returnResult(YOU_WIN_TWENTY_USD, { firstWheel, secondWheel, thirdWheel })
     }
 
     if (firstWheel !== secondWheel && secondWheel === thirdWheel) {
-        msg = YOU_WIN_TWENTY_USD
+        return returnResult(YOU_WIN_TWENTY_USD,{ firstWheel, secondWheel, thirdWheel })
     }
-
-    return (
-        <>
-            {msg && <>
-                    <h1>{msg}</h1>
-                    <WheelImage wheels={[firstWheel, secondWheel, thirdWheel]} />
-                </>
-            }
-            
-        </>
-    );
 }
 
 export default function Result({ path }) {
-    const location = useLocation();
+    const { state } = useLocation();
 
     return (
         <div className='result-container'>
             <div>
-                {checkWheels(location.state)}
+                {checkWheels(state)}
             </div>
             <p>
                 <span><Link to={path}>{PLAY_AGAIN}</Link></span>
